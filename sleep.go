a25765cae2c3476f06fdbb76c3e9aa5bfc6225a0
Jesse Brown <jabrown85@gmail.com>
2023-05-18 11:22:04 -0500

    stay up for longer for debugging

--- sleep.go
+++ sleep.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	if args := len(os.Args); args != 1 {
 		fmt.Println("Args are not allowed!")
+		fmt.Println("I will exit in 5 minutes with status code 1")
+		time.Sleep(5 * time.Minute)
 		os.Exit(1)
 	}
 	fmt.Println("Sleeping forever")
